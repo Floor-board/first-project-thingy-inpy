8ea7e79f36403c135e9aa448904d9bad625f833b
floor-board <giggamer.80@gmail.com>
2024-10-03 14:06:30 -0700

    New stuff added

--- webapp.py
+++ webapp.py
@@ -1,38 +1,46 @@
-from flask import Flask, url_for, render_template
+import random
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route("/p1")
-def render_page1():
-    return render_template('page1.html')
+#@app.route("/p1")
+#def render_page1():
+ #   return render_template('page1.html')
 
-@app.route("/p2")
-def render_page2():
-    return render_template('page2.html')
-    
-if __name__=="__main__":
-    app.run(debug=False)
-    
-@app.route("/")
-def render_main():
-    return render_template('home.html')
+
+
+seven_ten = [7,8,9,10]
 
 @app.route("/response")
 def render_response():
-    distance = request.args['meters'] 
+    distance = str(request.args['meters']) 
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
     if distance == '100m' or distance == '100 meters':
-        n = int(request.args['secounds'])
+        n = float(request.args['secounds'])
         min = n * 2 - 0.2
         max = n * 2 + 0.2
+        rt = [min, max]
+        RandomRt = random.choice(rt)
+        print(RandomRt)
     elif distance == '200m' or '200 meters':
-        n = int(request.args['secounds'])
-        time = n * 2 * 1.07
+        n = float(request.args['secounds'])
+        time = n * 2 + 4
+        rt = time
+    elif distance == '400m' or '400 meters':
+        n = float(request.args['secounds'])
+        time = n * 2 + random.choice(seven_ten)
+        rt = time
+    elif distance == '800m' or '800 meters':
+        n = float(request.args['secounds'])
+        time = n * 2 + random.choice(seven_ten)
+        rt = time
+    return render_template('index.html', New_Time = rt)
+
 if __name__=="__main__":
     app.run(debug=False)
